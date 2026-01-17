a40e5683f6efb796ccc00b8c888223739f799e6b
mnmehr04 <157523751+mnmehr04@users.noreply.github.com>
2026-01-17 18:22:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // later restrict to frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 app.use(express.json());
 
 // Serve images from the images folder
